b5e7390da7745635ff2697900ea4d50357b013c2
dercio-sinione <derciosinione@gmail.com>
2021-07-22 00:13:29 +0100

    Button Action

--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,20 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-      title: 'Get Started',
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Container(
-          color: Colors.green,
-          height: 500,
-          width: 500,
-          child: Center(
-            child: Container(
-              width: 200,
-              height: 200,
-              color: Colors.amber[600],
-            ),
-          ),
-        ),
-      )));
+    title: 'Get Started',
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      primarySwatch: Colors.purple,
+    ),
+    home: Scaffold(
+      appBar: AppBar(title: Text('Get Started')),
+      body: Center(
+        child: Text("CONTADOR \n 0", textAlign: TextAlign.center),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+    ),
+  ));
 }
