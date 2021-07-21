5b33a6d4d203e62b74a887993bcb6ec52c33a24b
dercio-sinione <derciosinione@gmail.com>
2021-07-22 00:58:04 +0100

    Contador

--- lib/main.dart
+++ lib/main.dart
@@ -4,18 +4,41 @@ void main() {
   runApp(MaterialApp(
     title: 'Get Started',
     debugShowCheckedModeBanner: false,
-    theme: ThemeData(
-      primarySwatch: Colors.purple,
-    ),
-    home: Scaffold(
-      appBar: AppBar(title: Text('Get Started')),
-      body: Center(
-        child: Text("CONTADOR \n 0", textAlign: TextAlign.center),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.add),
-      ),
-    ),
+    theme: ThemeData(primarySwatch: Colors.purple),
+    home: HomePage(),
   ));
+}
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int count = 0;
+
+  void fnCount() {
+    setState(() {
+      count++;
+    });
+    print(count);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+        appBar: AppBar(title: Text('Meu Primeiro App')),
+        body: Center(
+          child: Text("CONTADOR \n $count", textAlign: TextAlign.center),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => fnCount(),
+          child: Icon(Icons.add),
+        ),
+      ),
+    );
+  }
 }
