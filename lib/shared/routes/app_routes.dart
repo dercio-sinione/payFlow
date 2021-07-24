f2566b05f1b6257e6b111ce7beab07ab1accd335
dercio-sinione <derciosinione@gmail.com>
2021-07-24 22:15:07 +0100

    Routes

--- lib/shared/routes/app_routes.dart
+++ lib/shared/routes/app_routes.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:nl_pay_flow/modules/home/home_page.dart';
 import 'package:nl_pay_flow/modules/login/login_page.dart';
 import 'package:nl_pay_flow/modules/splash/splash_page.dart';
+import 'routes_config.dart';
 
 dynamic fnAppRoutes(BuildContext context) {
   return {
