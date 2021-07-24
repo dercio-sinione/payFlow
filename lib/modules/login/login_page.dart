a8328647facfbee2d8a279b6caf21365a4ae1414
dercio-sinione <derciosinione@gmail.com>
2021-07-24 13:08:35 +0100

    First component

--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nl_pay_flow/shared/themes/app_colors.dart';
 
 class LoginPage extends StatefulWidget {
   LoginPage({Key? key}) : super(key: key);
@@ -10,8 +11,15 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: null,
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      body: Stack(
+        children: [
+          Container(
+            color: AppColors.primary,
+          )
+        ],
+      ),
     );
   }
 }
