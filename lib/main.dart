035fc8cb014e3e6b1fd19716d8dd512e3da5f52d
dercio-sinione <derciosinione@gmail.com>
2021-07-24 17:15:26 +0100

    Working with Routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import 'package:nl_pay_flow/modules/home/home_page.dart';
+import 'package:nl_pay_flow/modules/login/login_page.dart';
+import 'package:nl_pay_flow/modules/splash/splash_page.dart';
 import 'package:nl_pay_flow/shared/themes/app_colors.dart';
 
 void main() => runApp(AppWidget());
@@ -11,9 +13,15 @@ class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Pay Flow',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(primaryColor: AppColors.primary),
-        home: HomePage());
+      title: 'Pay Flow',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primaryColor: AppColors.primary),
+      initialRoute: "/splash",
+      routes: {
+        "/splash": (context) => SplashPage(),
+        "/login": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+      },
+    );
   }
 }
