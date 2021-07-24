44e365d8b012d4e87b74997e2dcdc8b101776fe3
dercio-sinione <derciosinione@gmail.com>
2021-07-24 12:34:32 +0100

    Importing

--- lib/modules/splash/splash_page.dart
+++ lib/modules/splash/splash_page.dart
@@ -1,1 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:nl_pay_flow/shared/themes/app_colors.dart';
 
+class SplashPage extends StatelessWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.background,
+    );
+  }
+}
