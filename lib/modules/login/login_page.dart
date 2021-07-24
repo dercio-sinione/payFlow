d6e78a0fbad23a067174994fa47aa1b08d7dccdd
dercio-sinione <derciosinione@gmail.com>
2021-07-24 13:32:00 +0100

    Mini Logo

--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:nl_pay_flow/shared/themes/app_colors.dart';
+import 'package:nl_pay_flow/shared/themes/app_images.dart';
 
 class LoginPage extends StatefulWidget {
   LoginPage({Key? key}) : super(key: key);
@@ -14,14 +15,31 @@ class _LoginPageState extends State<LoginPage> {
     final size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: AppColors.background,
-      body: Stack(
-        children: [
-          Container(
-            color: AppColors.primary,
-            width: size.width,
-            height: size.height * 0.30,
-          )
-        ],
+      body: Container(
+        width: size.width,
+        height: size.height,
+        child: Stack(
+          children: [
+            Container(
+              color: AppColors.primary,
+              width: size.width,
+              height: size.height * 0.30,
+            ),
+            Positioned(
+              top: 40,
+              left: 0,
+              right: 0,
+              child: Image.asset(AppImages.person, width: 208, height: 373),
+            ),
+            Positioned(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [Image.asset(AppImages.logomini)],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
