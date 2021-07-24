1a83109fe44edca03008c181b0a4d9dc3742a1c7
dercio-sinione <derciosinione@gmail.com>
2021-07-24 13:50:44 +0100

    Creating Social Button

--- lib/shared/widgets/social_login/social_login_button.dart
+++ lib/shared/widgets/social_login/social_login_button.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:nl_pay_flow/shared/themes/app_images.dart';
+import 'package:nl_pay_flow/shared/themes/app_text_style.dart';
 
 class SocialLoginButton extends StatelessWidget {
   const SocialLoginButton({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Row(
+        children: [
+          Image.asset(AppImages.google),
+          Text(
+            "Entrar com Google",
+            style: AppTextStyles.buttonGray,
+          )
+        ],
+      ),
+    );
   }
 }
