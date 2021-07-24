0240bc66c41b071843adcf07d8273d9ae0763da5
dercio-sinione <derciosinione@gmail.com>
2021-07-24 23:00:23 +0100

    feat: Flutter Shared Preferences

--- lib/shared/auth/auth_controller.dart
+++ lib/shared/auth/auth_controller.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/widgets.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthController {
   var _isAuthenticated = false;
@@ -15,5 +16,18 @@ class AuthController {
       _isAuthenticated = false;
       Navigator.pushReplacementNamed(context, "/login");
     }
+  }
+
+  Future<void> saveUser(var user) async {
+    final instance = await SharedPreferences.getInstance();
+    await instance.setString("user", user);
+    return;
+  }
+
+  Future<void> currentUser(BuildContext context) async {
+    final instance = await SharedPreferences.getInstance();
+    final user = instance.get("user");
+    setUser(context, user);
+    return;
   }
 }
