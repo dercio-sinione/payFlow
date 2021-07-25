dec0eb050c445761f5c559e2cabe222395bb1895
dercio-sinione <derciosinione@gmail.com>
2021-07-25 22:39:57 +0100

    feat: List Boletos Controller

--- lib/shared/widgets/boleto_list/boleto_list_controller.dart
+++ lib/shared/widgets/boleto_list/boleto_list_controller.dart
@@ -1,5 +1,17 @@
 import 'package:nl_pay_flow/shared/models/boleto.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class BoletoListController {
   List<BoletoModel> boletos = <BoletoModel>[];
+
+  Future<void> getBoleto() async {
+    try {
+      final instance = await SharedPreferences.getInstance();
+      final response = instance.getStringList("boletos") ?? <String>[];
+      boletos = response.map((e) => BoletoModel.fromJson(e)).toList();
+    } catch (e) {
+      boletos = <BoletoModel>[];
+      print(e.toString());
+    }
+  }
 }
