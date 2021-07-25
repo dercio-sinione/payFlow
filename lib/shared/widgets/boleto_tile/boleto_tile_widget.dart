c852b27530740d5825736a4b8b7abb21556ae252
dercio-sinione <derciosinione@gmail.com>
2021-07-25 20:51:41 +0100

    feat: Creating Widget Boleto Tile > decorations

--- lib/shared/widgets/boleto_tile/boleto_tile_widget.dart
+++ lib/shared/widgets/boleto_tile/boleto_tile_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:nl_pay_flow/shared/models/boleto.dart';
+import 'package:nl_pay_flow/shared/themes/app_text_style.dart';
 
 class BoletoTileWidget extends StatelessWidget {
   final BoletoModel data;
@@ -8,7 +9,16 @@ class BoletoTileWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text(""),
+      title: Text(data.name!),
+      subtitle: Text("Vence em ${data.dueDate}"),
+      trailing: Text.rich(TextSpan(
+          text: " AOA",
+          style: AppTextStyles.trailingRegular,
+          children: [
+            TextSpan(
+                text: "${data.value!.toStringAsFixed(2)}",
+                style: AppTextStyles.trailingBold),
+          ])),
     );
   }
 }
