45a2944bbba896049c7cf270aa61e8f7c3c0580e
dercio-sinione <derciosinione@gmail.com>
2021-07-25 16:37:02 +0100

    feat: Insert Boleto Page > Add route

--- lib/shared/routes/app_routes.dart
+++ lib/shared/routes/app_routes.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:nl_pay_flow/modules/home/home_page.dart';
+import 'package:nl_pay_flow/modules/insert_boleto/insert_boleto_page.dart';
 import 'package:nl_pay_flow/modules/login/login_page.dart';
 import 'package:nl_pay_flow/modules/splash/splash_page.dart';
 import 'routes_config.dart';
@@ -10,5 +11,6 @@ dynamic fnAppRoutes(BuildContext context) {
     AppRoutes.splash: (context) => SplashPage(),
     AppRoutes.login: (context) => LoginPage(),
     AppRoutes.home: (context) => HomePage(),
+    AppRoutes.insertBoleto: (context) => InsertBoletoPage(),
   };
 }
