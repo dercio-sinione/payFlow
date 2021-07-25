999ee145c52466b55f86ce3af8e2bcfd3fd9fcf7
dercio-sinione <derciosinione@gmail.com>
2021-07-25 18:53:28 +0100

    feat: Insert Boleto Page > Add Masked Controllers for inputText

--- lib/modules/insert_boleto/insert_boleto_page.dart
+++ lib/modules/insert_boleto/insert_boleto_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_masked_text2/flutter_masked_text2.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:nl_pay_flow/shared/themes/app_colors.dart';
 import 'package:nl_pay_flow/shared/themes/app_text_style.dart';
@@ -6,13 +7,21 @@ import 'package:nl_pay_flow/shared/widgets/input_text/input_text_widget.dart';
 import 'package:nl_pay_flow/shared/widgets/set_label_buttons/set_label_buttons.dart';
 
 class InsertBoletoPage extends StatefulWidget {
-  const InsertBoletoPage({Key? key}) : super(key: key);
+  final String? barcode;
+  const InsertBoletoPage({Key? key, this.barcode}) : super(key: key);
 
   @override
   _InsertBoletoPageState createState() => _InsertBoletoPageState();
 }
 
 class _InsertBoletoPageState extends State<InsertBoletoPage> {
+  final moneyInputController = MoneyMaskedTextController(
+    leftSymbol: "AOA",
+    decimalSeparator: ",",
+  );
+  final dueDateInputController = MaskedTextController(mask: "00/00/0000");
+  final barcodeInputController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
