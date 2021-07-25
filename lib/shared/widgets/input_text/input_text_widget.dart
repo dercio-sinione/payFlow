6a75bb10b2b40b416b80373706448ca4ebc6fb78
dercio-sinione <derciosinione@gmail.com>
2021-07-25 17:16:31 +0100

    feat: Insert Boleto Page > Add mainAxisSize into input to show what we are writing

--- lib/shared/widgets/input_text/input_text_widget.dart
+++ lib/shared/widgets/input_text/input_text_widget.dart
@@ -14,6 +14,7 @@ class InputTextWidget extends StatelessWidget {
           decoration: InputDecoration(
             border: InputBorder.none,
             icon: Row(
+              mainAxisSize: MainAxisSize.min,
               children: [
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 18),
