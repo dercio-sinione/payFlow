f3aef025d2004ea18040c21b1b8a81989b4b3271
dercio-sinione <derciosinione@gmail.com>
2021-07-25 23:17:19 +0100

    feat: Meus Boletos Page

--- lib/shared/widgets/boleto_info/boleto_info_widget.dart
+++ lib/shared/widgets/boleto_info/boleto_info_widget.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:nl_pay_flow/shared/themes/app_colors.dart';
+import 'package:nl_pay_flow/shared/themes/app_images.dart';
+import 'package:nl_pay_flow/shared/themes/app_text_style.dart';
 
 class BoletoInfoWidget extends StatelessWidget {
   final int size;
@@ -8,7 +10,34 @@ class BoletoInfoWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(color: AppColors.body),
+      decoration: BoxDecoration(
+        color: AppColors.secondary,
+        borderRadius: BorderRadius.circular(5),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Image.asset(
+            AppImages.logomini,
+            color: AppColors.background,
+            width: 56,
+            height: 34,
+          ),
+          Container(
+            width: 1,
+            height: 32,
+            color: AppColors.background,
+          ),
+          Text.rich(TextSpan(
+              text: "Você tem",
+              style: AppTextStyles.captionBoldBackground,
+              children: [
+                TextSpan(
+                    text: "$size boletos",
+                    style: AppTextStyles.captionBackground)
+              ]))
+        ],
+      ),
     );
   }
 }
