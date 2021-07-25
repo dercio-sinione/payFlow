7185fa0f7c5e17090f58d41297f76ac8610bdd83
dercio-sinione <derciosinione@gmail.com>
2021-07-25 02:15:57 +0100

    feat: Home Page

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:nl_pay_flow/shared/themes/app_colors.dart';
+import 'package:nl_pay_flow/shared/themes/app_text_style.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -6,8 +8,27 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Home Page"),
+      appBar: PreferredSize(
+        child: Container(
+          height: 152,
+          color: AppColors.primary,
+          child: Center(
+            child: ListTile(
+              title: Text("Olá, Dércio", style: AppTextStyles.captionShape),
+              subtitle: Text("Mantenha as suas contas em dia.",
+                  style: AppTextStyles.captionShape),
+              trailing: Container(
+                width: 48,
+                height: 40,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+            ),
+          ),
+        ),
+        preferredSize: Size.fromHeight(152),
       ),
     );
   }
