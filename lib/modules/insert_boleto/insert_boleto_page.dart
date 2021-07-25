dd5cf362dbb8b482c6d51f88c2d4fa974ef88028
dercio-sinione <derciosinione@gmail.com>
2021-07-25 16:43:16 +0100

    feat: Insert Boleto Page > Change Background

--- lib/modules/insert_boleto/insert_boleto_page.dart
+++ lib/modules/insert_boleto/insert_boleto_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nl_pay_flow/shared/themes/app_colors.dart';
 
 class InsertBoletoPage extends StatelessWidget {
   const InsertBoletoPage({Key? key}) : super(key: key);
@@ -6,7 +7,10 @@ class InsertBoletoPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.background,
       appBar: AppBar(
+        backgroundColor: AppColors.background,
+        elevation: 0,
         title: Text('Insert Boleto Page'),
       ),
     );
