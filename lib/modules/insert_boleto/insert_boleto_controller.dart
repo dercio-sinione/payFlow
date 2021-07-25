f4f3929940fd022fc8f68ca265ca4394b854a761
dercio-sinione <derciosinione@gmail.com>
2021-07-25 19:12:07 +0100

    feat: Insert Boleto Page > Insert Boleto Controller

--- lib/modules/insert_boleto/insert_boleto_controller.dart
+++ lib/modules/insert_boleto/insert_boleto_controller.dart
@@ -4,7 +4,10 @@ class InsertBoletoController {
 
   String? validateVencimento(String? value) =>
       value?.isEmpty ?? true ? "A data de vencimento não pode ser vazio" : null;
-  
-    String? validateValor(String? value) =>
-      value==0 ? "Insira um valor a cima" : null;
+
+  String? validateValor(String? value) =>
+      value == 0 ? "Insira um valor maior que 0,00 AOA" : null;
+
+  String? validateCodigo(String? value) =>
+      value?.isEmpty ?? true ? "O código de barra não pode ser vazio" : null;
 }
