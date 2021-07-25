d5388e6a9dc93e825bb72a72485751b3fcd3a4d8
dercio-sinione <derciosinione@gmail.com>
2021-07-25 22:45:16 +0100

    feat: List Boletos Controller

--- lib/shared/widgets/boleto_list/boleto_list_controller.dart
+++ lib/shared/widgets/boleto_list/boleto_list_controller.dart
@@ -5,7 +5,9 @@ import 'package:shared_preferences/shared_preferences.dart';
 class BoletoListController {
   final boletosNotifier = ValueNotifier<List<BoletoModel>>(<BoletoModel>[]);
   List<BoletoModel> get boletos => boletosNotifier.value;
-  
+
+  set boletos(List<BoletoModel> value) => boletosNotifier.value = value;
+
   BoletoListController() {
     getBoletos();
   }
