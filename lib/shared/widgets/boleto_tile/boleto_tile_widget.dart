df6d7cef2a294670be05632014a924ba010d1196
dercio-sinione <derciosinione@gmail.com>
2021-07-26 01:41:32 +0100

    feat: Meus Boletos Page > Add Animations Dine

--- lib/shared/widgets/boleto_tile/boleto_tile_widget.dart
+++ lib/shared/widgets/boleto_tile/boleto_tile_widget.dart
@@ -1,3 +1,4 @@
+import 'package:animated_card/animated_card.dart';
 import 'package:flutter/material.dart';
 import 'package:nl_pay_flow/shared/models/boleto.dart';
 import 'package:nl_pay_flow/shared/themes/app_text_style.dart';
@@ -8,17 +9,20 @@ class BoletoTileWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      contentPadding: EdgeInsets.zero,
-      title: Text(data.name!, style: AppTextStyles.titleListTile),
-      subtitle:
-          Text("Vence em ${data.dueDate}", style: AppTextStyles.captionBody),
-      trailing: Text.rich(TextSpan(
-          text: "${data.value!.toStringAsFixed(2)}",
-          style: AppTextStyles.trailingBold,
-          children: [
-            TextSpan(text: " AOA", style: AppTextStyles.trailingRegular),
-          ])),
+    return AnimatedCard(
+      direction: AnimatedCardDirection.right,
+      child: ListTile(
+        contentPadding: EdgeInsets.zero,
+        title: Text(data.name!, style: AppTextStyles.titleListTile),
+        subtitle:
+            Text("Vence em ${data.dueDate}", style: AppTextStyles.captionBody),
+        trailing: Text.rich(TextSpan(
+            text: "${data.value!.toStringAsFixed(2)}",
+            style: AppTextStyles.trailingBold,
+            children: [
+              TextSpan(text: " AOA", style: AppTextStyles.trailingRegular),
+            ])),
+      ),
     );
   }
 }
